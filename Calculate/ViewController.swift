a0c270a0be1656385003522ae67808ad82e3cf74
Baris Ciftci <barishciftci@gmail.com>
2022-09-06 21:00:30 +0200

    AC button function has been set

--- Calculate/ViewController.swift
+++ Calculate/ViewController.swift
@@ -17,6 +17,7 @@ class ViewController: UIViewController
     
     @IBOutlet weak var displayLabel: UILabel!
     
+    var shouldReset = true
     var displayString = "0"
     
     override func viewDidLoad()
@@ -24,84 +25,175 @@ class ViewController: UIViewController
         super.viewDidLoad()
         displayLabel.text = displayString
     }
+    
     @IBAction func zeroButtonTapped(_ sender: UIButton)
     {
         
         let zero = "0"
         
-        displayString.append(zero)
-        displayLabel.text = displayString
+        if zero == "0" && displayString == "0" {return}
         
+        if (shouldReset)
+        {
+            displayString = zero
+            shouldReset = false
+        }
+        else
+        {
+            displayString.append(zero)
+        }
+    
+        displayLabel.text = displayString
     }
+    
     @IBAction func oneButtonTapped(_ sender: UIButton)
     {
         let one = "1"
+        if (shouldReset)
+        {
+            displayString = one
+            shouldReset = false
+        }
+        else
+        {
+            displayString.append(one)
+        }
         
-        displayString.append(one)
         displayLabel.text = displayString
     }
+    
     @IBAction func twoButtonTapped(_ sender: UIButton)
     {
         let two = "2"
-        
-        displayString.append(two)
+        if (shouldReset)
+        {
+            displayString = two
+            shouldReset = false
+        }
+        else
+        {
+            displayString.append(two)
+        }
         displayLabel.text = displayString
     }
+    
     @IBAction func threeButtonTapped(_ sender: UIButton)
     {
         let three = "3"
         
-        displayString.append(three)
+        if (shouldReset)
+        {
+            displayString = three
+            shouldReset = false
+        }
+        else
+        {
+            displayString.append(three)
+        }
         displayLabel.text = displayString
     }
+    
     @IBAction func fourButtonTapped(_ sender: UIButton)
     {
         let four = "4"
         
-        displayString.append(four)
+        if (shouldReset)
+        {
+            displayString = four
+            shouldReset = false
+        }
+        else
+        {
+            displayString.append(four)
+        }
         displayLabel.text = displayString
     }
+    
     @IBAction func fiveButtonTapped(_ sender: UIButton)
     {
         let five = "5"
         
-        displayString.append(five)
+        if (shouldReset)
+        {
+            displayString = five
+            shouldReset = false
+        }
+        else
+        {
+            displayString.append(five)
+        }
         displayLabel.text = displayString
     }
+    
     @IBAction func sixButtonTapped(_ sender: UIButton)
     {
         let six = "6"
         
-        displayString.append(six)
+        if (shouldReset)
+        {
+            displayString = six
+            shouldReset = false
+        }
+        else
+        {
+            displayString.append(six)
+        }
         displayLabel.text = displayString
     }
+    
     @IBAction func sevenButtonTapped(_ sender: UIButton)
     {
         let seven = "7"
         
-        displayString.append(seven)
+        if (shouldReset)
+        {
+            displayString = seven
+            shouldReset = false
+        }
+        else
+        {
+            displayString.append(seven)
+        }
         displayLabel.text = displayString
     }
+    
     @IBAction func eightButtonTapped(_ sender: UIButton)
     {
         let eight = "8"
         
-        displayString.append(eight)
+        if (shouldReset)
+        {
+            displayString = eight
+            shouldReset = false
+        }
+        else
+        {
+            displayString.append(eight)
+        }
         displayLabel.text = displayString
     
     }
+    
     @IBAction func nineButtonTapped(_ sender: UIButton)
     {
         let nine = "9"
-        
-        displayString.append(nine)
+       
+        if (shouldReset)
+        {
+            displayString = nine
+            shouldReset = false
+        }
+        else
+        {
+            displayString.append(nine)
+        }
         displayLabel.text = displayString
     }
-    
-    
+
     @IBAction func acButtonTapped(_ sender: UIButton)
     {
         displayString = "0"
+        shouldReset = true
         displayLabel.text = displayString
     }
    
